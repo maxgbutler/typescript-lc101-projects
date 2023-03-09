3b6a2cbebc279ef54c061d502d0f455f9f274fd7
Max Butler <maxgbutler003@gmail.com>
2023-03-08 19:20:09 -0600

    Studio and Exercise solutions

--- exercises/parts1-5.js
+++ exercises/parts1-5.js
@@ -1,76 +1,45 @@
 // URL for the instructions: 
 // https://education.launchcode.org/intro-to-professional-web-dev/chapters/typescript/exercises.html 
-
-
 // Part 1: Declare (5) Variables With Type
-let kilometersToMars: number = 225000000;
-let kilometersToMoon: number = 384400;
-
-
+var kilometersToMars = 225000000;
+var kilometersToMoon = 384400;
 // Part 2: Print Days to Mars
 // let milesToMars: number = kilometersToMars * milesPerKilometer;
 // let hoursToMars: number = milesToMars / speedMph;
 // let daysToMars: number = hoursToMars / 24;
-
-
 // Code an output statement here (use a template literal):
-
-
-
 // Part 3: Create a Function ("getDaysToLotascation")
 // function getDaysToLocation(kilometersAway: number): number {
 //     let milesToLocation: number = kilometersAway * milesPerKilometer;
 //     let hoursToLocation: number = milesToLocation / speedMph;
 //     let daysToLoaction: number = hoursToLocation / 24;
-    
 //     return daysToLoaction;
 // };
-
-
 // Move your output statement from part 2 here. Update the template literal to call
 // the function and print the outputs for a Mars trip and a moon trip.
-
-
-
-
-
 // Part 4: Create a Spacecraft Class
-class Spacecraft {
-    milesPerKilometer: number = 0.621;
-    name: string;
-    speedMph: number;
-    constructor( name: string, speedMph: number) {
+var Spacecraft = /** @class */ (function () {
+    function Spacecraft(name, speedMph) {
+        this.milesPerKilometer = 0.621;
         this.name = name;
         this.speedMph = speedMph;
     }
-
-    getDaysToLocation(kilometersAway: number): number {
-        let milesToLocation: number = kilometersAway * this.milesPerKilometer;
-        let hoursToLocation: number = milesToLocation / this.speedMph;
-        let daysToLoaction: number = hoursToLocation / 24;
-        
+    Spacecraft.prototype.getDaysToLocation = function (kilometersAway) {
+        var milesToLocation = kilometersAway * this.milesPerKilometer;
+        var hoursToLocation = milesToLocation / this.speedMph;
+        var daysToLoaction = hoursToLocation / 24;
         return daysToLoaction;
     };
-}
-
-let spaceShuttle = new Spacecraft('Determination', 17500);
-
-console.log( `It takes ${spaceShuttle.getDaysToLocation(kilometersToMars)} days to reach Mars aboard the ${spaceShuttle.name}` );
-console.log( `It takes ${spaceShuttle.getDaysToLocation(kilometersToMoon)} days to reach the Moon aboard the ${spaceShuttle.name}` );
-
+    ;
+    return Spacecraft;
+}());
+var spaceShuttle = new Spacecraft('Determination', 17500);
+console.log("It takes ".concat(spaceShuttle.getDaysToLocation(kilometersToMars), " days to reach Mars aboard the ").concat(spaceShuttle.name));
+console.log("It takes ".concat(spaceShuttle.getDaysToLocation(kilometersToMoon), " days to reach the Moon aboard the ").concat(spaceShuttle.name));
 // Create an instance of the class here:
-
-
-
 // Move your output statements from part 3 here. Update the template literals use the
 // instance of the class.
-
-
-
 // Part 5: Export and Import the SpaceLocation Class
 // Add the required import statement BEFORE the part 1 concent.
-
-
 // Add the printDaysToLocation function to the Spacecraft class.
-
 // Paste in the code from step 6 here:
---
+++ studio/Rocket.ts
@@ -0,0 +1,51 @@
+import { Payload } from './Payload';
+import { Cargo } from './Cargo';
+import { Astronaut } from './Astronaut';
+
+export class Rocket {
+    name: string;
+    totalCapacityKg: number;
+    cargoItems: Cargo[] = [];
+    astronauts: Astronaut[] = [];
+    constructor( name: string, totalCapacityKg: number ) {
+        this.name = name;
+        this.totalCapacityKg = totalCapacityKg;
+    }
+
+    sumMass( items: Payload[] ): number {
+        let totalMass: number = 0;
+        for ( let item in items ) {
+            totalMass += items[item].massKg;
+        }
+        return totalMass;
+    }
+
+    currentMassKg(): number {
+        return this.sumMass( this.cargoItems ) + this.sumMass( this.astronauts );
+    }
+
+    canAdd( item: Payload ): boolean {
+        if ( this.currentMassKg() + item.massKg <= this.totalCapacityKg ) 
+        return true;
+
+        return false;
+    }
+
+    addCargo( cargo: Cargo ): boolean {
+        if( this.canAdd( cargo ) ) {
+            this.cargoItems.push( cargo );
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    addAstronaut(astronaut: Astronaut): boolean {
+        if( this.canAdd( astronaut ) ) {
+            this.astronauts.push( astronaut );
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
